cbabb02fb7238c53f66a32d8a6eb80d74b7958fc
akirmas <andrii@gobe-mark.com>
2018-12-24 15:55:32 +0200

    All 2

--- Isracard/dump2.php
+++ Isracard/dump2.php
@@ -1,4 +1,9 @@
 <?php
+ini_set("implicit_flush", 1);
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+ini_set('max_execution_time', 0);
+while (@ob_end_flush());
+ob_implicit_flush(true);
 require_once('index.php');
 
 $psp = new Isracard('test');
@@ -6,18 +11,18 @@ $psp = new Isracard('test');
 $deal = new Deal($_REQUEST);
 $contact = new Contact($_REQUEST);
 
-$dealId = time();
-$deal['id'] = $dealId;
+$dealId = 'IC' . time();
+$deal->id = $dealId;
 
 $url = $psp->payUrl(
   $deal,
-  "http://payment.gobemark.info/php/psps/collector.php",
+  "https://payment.gobemark.info/php/psps/collector.php?fireid=$dealId&",
   $contact
 );
-
-$nodeJS = new SyncEvent("Response_Deal$dealId");
-echo $url;
-flush() ;
+header('HTTP/1.1 206 Partial Content');
+echo json_encode(array('url' => $url));
+flush();
 ob_flush();
+$nodeJS = new SyncEvent($dealId);
 $nodeJS->wait();
-echo "Response+";
+echo ','.file_get_contents("../responses/$dealId.json");
--- collector.php
+++ collector.php
@@ -1,8 +1,7 @@
 <?php
-$log = fopen('./responses/'.date('ymd-His-').rand().'.json', 'w');
+$fireId = $_REQUEST['fireid'];
+$log = fopen("./responses/$fireId.json", 'w');
 fwrite($log, json_encode($_REQUEST, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
 fclose($log);
-echo 'firing';
-$nodeJS = new SyncEvent("Response_Deal$dealId");
+$nodeJS = new SyncEvent($fireId);
 $nodeJS->fire();
-echo 'fired';
