4ed34a3ee9346a0bb71fa8760f5ffb02e37eb9ce
akirmas <andrii@gobe-mark.com>
2018-12-30 12:37:51 +0200

    Add contact's names calc

--- PSP/Contact.php
+++ PSP/Contact.php
@@ -5,7 +5,6 @@ require_once(__DIR__.'/Assoc.php');
 class Contact extends Assoc {
   /** @var string */
   public $first_name;
-  public $middle_name;
   public $last_name;
   public $full_name;
   public $email;
@@ -15,5 +14,13 @@ class Contact extends Assoc {
 
   function __construct($assoc) {
     parent::__construct($assoc, $this);
+    if (empty($this->full_name))
+      $this->full_name = join(' ', [$this->first_name, $this->last_name]);
+    elseif (empty($this->first_name) || empty($this->last_name)) {
+      $names = explode(' ', $this->full_name);
+      $this->last_name = array_pop($names);
+      $this->first_name = array_shift($names);
+      $this->last_name = join(' ', $names + [$this->last_name]);
+    }
   }
 }
