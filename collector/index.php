fa6a7d69ad965d5ab69a08a1ca0e33bee6b53dff
akirmas <andrii@gobe-mark.com>
2018-12-31 11:01:18 +0200

    Delete pretiffy of collector

--- collector/index.php
+++ collector/index.php
@@ -4,7 +4,7 @@ require_once(__DIR__.'/Collector.php');
 if (array_key_exists(Collector::fireField, $_REQUEST)) {
   $fireId = $_REQUEST[Collector::fireField];
   $eventContent = fopen(Collector::contentPath($fireId), 'w');
-  fwrite($eventContent, json_encode($_REQUEST, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
+  fwrite($eventContent, json_encode($_REQUEST));
   fclose($eventContent);
   $nodeJS = new SyncEvent($fireId);
   $nodeJS->fire();
