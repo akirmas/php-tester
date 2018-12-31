b6ec80dd9e671f78236cd3c25d938885235a3ec9
akirmas <andrii@gobe-mark.com>
2018-12-31 12:43:35 +0200

    Add envs to psps

--- index.php
+++ index.php
@@ -8,48 +8,64 @@ require_once(__DIR__.'/collector/Collector.php');
 $request = sizeof($_REQUEST) !== 0 ? $_REQUEST
 : json_decode(file_get_contents('php://input'), true);
 
-$request = array(
-  'email' => 'x@gbm',
-  //'cc_number' => '4319462718879596',
-  'cc_number' => '12312312',
-  'cc_expire' => '0820',
-  'cc_cvv' => '662',
-  'amount' => '149',
-  'currency' => 'USD',
-  'description' => 'Marketscap.+Single+Page+App.+Payment.+149+pounds.+Nov+2018+DF0F31AA2988C0ABD3CC34E249477438',
-  'verify' => 1,
-  'full_name' => 'Gbm Test',
-  'id' => date('Ymd_His'),
-  'account' => 'x'
-);
-
-$tr = new Tranzila;
-$ic = new Isracard;
-$contact = new Contact($request);
-$creditCard = new CreditCard($request);
-$transaction = new Transaction($request);
-$dealUSD = new Deal($request);
-$dealNIS = new Deal($request);
-$dealNIS->currency = 'NIS';
-$dealUSD->currency = 'USD';
-
-$collector = new Collector("tric$transaction->id");
-/*print_r([
-  $_SERVER['HTTP_HOST'], $_SERVER['SERVER_NAME'],
-  "---TR---",
-  $tr->instant('test', $transaction, $dealNIS, $contact, $creditCard),
-  "---IC---",
-  $ic->iframe('test', $transaction, $dealNIS, '', '', $contact),
-  "---IC---",
-  $ic->iframe('test', $transaction, $dealUSD, '', '', $contact)
-]);*/
-$result = $ic->iframe('test', $transaction, $dealUSD, $collector->callbackUrl, $collector->callbackUrl, $contact);
-if ($result['success']) {
-  echo json_encode(array('cbUrl' => $collector->callbackUrl, 'iframe' => $result['iframe']));
-  $content = $collector->wait();
-  echo ',';
-  echo json_encode(array('result' => $content));
-  exit;
+if (!array_key_exists('env', $request)) {
+  echo 'No env';
+  exit(1);
 }
 
+$envPath = __DIR__.'/envs/'.$request['env'].'.json';
+if (!file_exists($envPath)) {
+  echo 'No env config';
+  exit(2);
+}
 
+$env = json_decode(file_get_contents($envPath), "true");
+forEach($env as $step) {
+  $pspName = $step['psp'];
+  switch ($pspName) {
+    case 'Tranzila':
+      $psp = new Tranzila;
+      break;
+    case 'Isracard':
+      $psp = new Isracard;
+      break;
+    default:
+      echo "Not implemented: '$pspName'";
+      exit(3);
+  }
+
+  $method = $step['method'];
+  $env = $step['env'];
+  $contact = new Contact($request);
+  $creditCard = new CreditCard($request);
+  $transaction = new Transaction($request);
+  $deal = new Deal($request);
+
+  $result;
+  switch($method) {
+    case 'iframe':
+      $result = $psp->iframe($env, $transaction, $deal, '', $collector->callbackUrl, $contact);
+      break;
+    case 'iframeContinued':
+      $collector = new Collector("tr_$_$transaction->id");
+      $result = $psp->iframe($env, $transaction, $deal, '', $collector->callbackUrl, $contact);
+      echo json_encode(array('iframe' => $result['iframe']));
+      $result = $collector->wait();
+      echo ',';    
+      break;
+    case 'instant':
+      $transaction->verify = 1;
+      $result = $tr->instant($env, $transaction, $dealNIS, $contact, $creditCard);
+      if (!$result['success']) {
+        echo $result['message'];
+        exit();
+      }
+      $transaction->verify = 0;
+      $result = $tr->instant($env, $transaction, $dealNIS, $contact, $creditCard);
+      break;
+    default:
+      echo "Not implemented: '$pspName->$method'";
+      exit(4);
+  }    
+}
+echo json_encode(array('result' => $result));
