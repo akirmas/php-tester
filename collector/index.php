d53db75cd5b3e3be7f282dbf303eb9f747cbc947
Andrii Kirmas <kirmas@phystech.edu>
2019-01-13 17:02:40 +0200

    Fix mkdir issues

--- collector/index.php
+++ collector/index.php
@@ -9,19 +9,23 @@ if (array_key_exists(Collector::fireField, $_REQUEST)) {
   $nodeJS = new SyncEvent($fireId);
   $nodeJS->fire();
 } else {
-  $request = array_merge($_REQUEST, ['ip' => $_SERVER['REMOTE_ADDR']]);
+  $request = array_merge($_REQUEST, ['ip' => !array_key_exists('REMOTE_ADDR', $_SERVER) ? '' : $_SERVER['REMOTE_ADDR']]);
 
   $instance = !array_key_exists('instance', $request) ? 'unknown' : $request['instance'];
-  $instanceDir = __DIR__."/collected/$instance";
-  if (!file_exists($instanceDir)) mkdir($instanceDir);
+  $instanceDir = mkdir2(__DIR__, 'collected', $instance);
 
   $eventId = $instance === 'Netpay' ? $request['reference'] : 'unknown-'.date('Ymd-His');
-  $eventDir = "$instanceDir/$eventId";
-  if (!file_exists($eventDir)) mkdir($eventDir); 
+  $eventDir = mkdir2($instanceDir, $eventId);
 
   file_put_contents("$eventDir/index.json", json_encode($request, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
   file_put_contents("$eventDir/".rand().".json", json_encode($request, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
   $event = new SyncEvent("$instance/$eventId");
   $event->fire();
+}
+
+function mkdir2(...$folders) {
+  $dir = join(DIRECTORY_SEPARATOR, $folders);
+  if (!file_exists($dir)) mkdir($dir, 0777, true);
+  return $dir;
 }
