191e19322dd32dc8fe3175cea31403ab3bce1118
Andrii Kirmas <kirmas@phystech.edu>
2019-01-13 19:48:35 +0200

    Fix forgotten slash

--- await/index.php
+++ await/index.php
@@ -7,7 +7,7 @@ header('Access-Control-Allow-Methods: POST, GET');
 $request = $_REQUEST;
 $eventName = $request['event'];
 
-$eventFile = __DIR__."../collected/$eventName/index.json";
+$eventFile = __DIR__."/../collected/$eventName/index.json";
 if (!file_exists($eventFile)) {
   $event = new SyncEvent($eventName);
   $event->wait();
