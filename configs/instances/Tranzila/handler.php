a206b6e8db00ad10d69babc505eafd824ed7e879
akirmas <andrii@gobe-mark.com>
2019-01-21 17:25:11 +0200

    Add 'success' to tranzila

--- configs/instances/Tranzila/handler.php
+++ configs/instances/Tranzila/handler.php
@@ -3,7 +3,10 @@ require_once(__DIR__.'/../../../CycleHandler.php');
 
 class Tranzila extends CycleHandler {
   static function onResponseRaw(object $env, object $response, object $request): object{
-    return (object) array('ResponseClone' => $response->Response);
+    return (object) [
+      'ResponseClone' => $response->Response,
+      'ResponseClone2' => $response->Response
+    ];
   }
 }
 
