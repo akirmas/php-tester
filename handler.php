da9318d203ae90ba727b6efa804b9e2fe6376693
akirmas <andrii@gobe-mark.com>
2019-01-21 17:22:58 +0200

    Add fee and names calculations

--- handler.php
+++ handler.php
@@ -11,18 +11,6 @@ class CommonHandler extends CycleHandler {
     : ''
     );
 
-    // TODO: Fee 
-    $amount = $input->amount;
-    $currency = $input->currency;
-    if (property_exists($input, 'currency:final') && ($currency != $input->{'currency:final'})) {
-      $pair = $currency.'_'.$input->{'currency:final'};
-      $rate = json_decode(file_get_contents(
-        "https://free.currencyconverterapi.com/api/v5/convert?q=$pair&compact=y"
-      ))->pair->val;
-      $amount = $input->amount * $rate;
-      $currency = $input->{'currency:final'};
-    }
-
     $name_full = (property_exists($input, 'name:full'))
     ? $input->{'name:full'}
     : trim(
@@ -31,12 +19,37 @@ class CommonHandler extends CycleHandler {
       .(!(property_exists($input, 'name:last')) ? '' : $input->{'name:last'})
     );
     
+    $names = explode(' ', $name_full);
+    $name_last = array_pop($names);
+    $name_first = sizeof($names) > 0 ? array_shift($names) : '';
+    $name_last = join(' ', array_merge($names, [$name_last]));
     return (object) [
       'cc:expire:date' => $date,
+      'name:full' => $name_full,
+      'name:first' => $name_first,
+      'name:last' => $name_last
+    ];
+  }
+  static function onRequestFormed(object $env, object $request): object {
+    $amount = $request->amount;
+    $currency = $request->currency;
+    $fee = !property_exists($request, 'fee') ? 0 : (float) $request->fee;
+    if (
+      property_exists($request, 'currency:final')
+      && ($currency != $request->{'currency:final'})
+    ) {
+      $pair = $currency.'_'.$request->{'currency:final'};
+      $rate = json_decode(file_get_contents(
+        "https://free.currencyconverterapi.com/api/v5/convert?q=$pair&compact=y"
+      ))->{$pair}->val;
+      $amount = $request->amount * $rate * (1 + $fee);
+      $currency = $request->{'currency:final'};
+    }
+    return (object) [
       'amount:final' => $amount,
       'amountInt:final' => 100 * (float) $amount,
       'currency:final' => $currency,
-      'name:full' => $name_full
+      'fee:final' => 0
     ];
   }
   static function onResponseFormed(object $env, object $output, object $input) : object {
