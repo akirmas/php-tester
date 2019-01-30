9bfd7ff68054ed1c4faab3a53c476ac39ff4bedf
Andrii Shykov <blue8sky@gmail.com>
2019-01-30 19:24:29 +0200

    Add tests for Opis' Validator. #5

--- tests/unit/OpisValidatorTest.php
+++ tests/unit/OpisValidatorTest.php
@@ -11,6 +11,7 @@ class OpisValidatorTest extends \Codeception\Test\Unit
     
     protected function _before()
     {
+        $this->_validator = new Opis\JsonSchema\Validator();
     }
 
     protected function _after()
@@ -26,30 +27,171 @@ class OpisValidatorTest extends \Codeception\Test\Unit
 
     public function testSchemaValidationMethodReturnsInstanceOfValidationResultClass()
     {
-        $validator = new Opis\JsonSchema\Validator();
         $data = json_decode('{}');
         $schema = \Opis\JsonSchema\Schema::fromJsonString('{}');
-        $result = $validator->schemaValidation($data, $schema);
+        $result = $this->_validator->schemaValidation($data, $schema);
         $this->assertInstanceOf('Opis\JsonSchema\ValidationResult', $result);
     }
 
     public function testSchemaValidationMethodReturnsNotIsValidResult()
     {
-        $validator = new Opis\JsonSchema\Validator();
         $data = json_decode('{ "name": [] }');
         $schema = \Opis\JsonSchema\Schema::fromJsonString('{ "type": "object", "properties": { "name": { "type": "string" } } }');
-        $result = $validator->schemaValidation($data, $schema);
+        $result = $this->_validator->schemaValidation($data, $schema);
         $this->assertFalse($result->isValid());
     }
 
 
     public function testSchemaValidationMethodReturnsIsValidResult()
     {
-        $validator = new Opis\JsonSchema\Validator();
         $data = json_decode('{ "name": "Andrii" }');
         $schema = \Opis\JsonSchema\Schema::fromJsonString('{ "type": "object", "properties": { "name": { "type": "string" } } }');
-        $result = $validator->schemaValidation($data, $schema);
+        $result = $this->_validator->schemaValidation($data, $schema);
         $this->assertTrue($result->isValid());
+    }
+
+    public function testAdditionalPropertiesForbiddenButPresent()
+    {
+        $data = json_decode('{ "name": "Andrii", "surname": "Shykov" }');
+        $schemaString = '{ "type": "object",
+                               "properties": {
+                                    "name": { "type": "string" }
+                               },
+                               "additionalProperties": false
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertFalse($result->isValid());
+    }
+
+    public function testAdditionalPropertiesForbiddenAndNotPresent()
+    {
+        $data = json_decode('{ "name": "Andrii" }');
+        $schemaString = '{ "type": "object",
+                               "properties": {
+                                    "name": { "type": "string" }
+                               },
+                               "additionalProperties": false
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertTrue($result->isValid());
+    }
+
+    public function testRequiredPropertiesNotPresent()
+    {
+        $data = json_decode('{ "name": "Andrii" }');
+        $schemaString = '{ "type": "object",
+                               "properties": {
+                                    "name": { "type": "string" }
+                               },
+                               "required": ["surname"]
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertFalse($result->isValid());
+    }
+
+    public function testRequiredPropertiesPresent()
+    {
+        $data = json_decode('{ "name": "Andrii", "surname": "Shykov" }');
+        $schemaString = '{ "type": "object",
+                               "properties": {
+                                    "name": { "type": "string" }
+                               },
+                               "required": ["surname"]
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertTrue($result->isValid());
+    }
+
+    public function testEnumPropertiesPresent()
+    {
+        $data = json_decode('{ "name": "Andrii" }');
+        $schemaString = '{ "type": "object",
+                               "properties": {
+                                    "name": { "enum": ["Andrii", "Alex"] }
+                               }
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertTrue($result->isValid());
+    }
+
+    public function testEnumPropertiesIsNotPresent()
+    {
+        $data = json_decode('{ "name": "Sasha" }');
+        $schemaString = '{ "type": "object",
+                            "properties": {
+                                    "name": { "enum": ["Andrii", "Alex"] }
+                               }
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertFalse($result->isValid());
+    }
+
+
+    public function testFormatUriForValidUri()
+    {
+        $data = json_decode('{ "gateway": "http://google.com" }');
+        $schemaString = '{ "type": "object",
+                               "properties": {
+                                    "gateway": { "type": "string", "format": "uri" }
+                               }
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertTrue($result->isValid());
+    }
+
+    public function testFormatUriForNotValidUri()
+    {
+        $data = json_decode('{ "gateway": "~http://google.com" }');
+        $schemaString = '{ "type": "object",
+                               "properties": {
+                                    "gateway": { "type": "string", "format": "uri" }
+                               }
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertFalse($result->isValid());
+    }
+
+
+    public function testPropertyNamesForValidPropertyNames()
+    {
+        $data = json_decode('{ "gateway": "http://google.com" }');
+        $schemaString = '{ "type": "object",
+                           "propertyNames": {
+                                "type": "string",
+                                "minLength": 4
+                           },
+                               "properties": {
+                                    "gateway": { "type": "string" }
+                               }
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertTrue($result->isValid());
+    }
+
+    public function testPropertyNamesForInvalidPropertyNames()
+    {
+        $data = json_decode('{ "url": "http://google.com" }');
+        $schemaString = '{ "type": "object",
+                           "propertyNames": {
+                                "type": "string",
+                                "minLength": 4
+                           },
+                               "properties": {
+                                    "gateway": { "type": "string" }
+                               }
+                            }';
+        $schema = \Opis\JsonSchema\Schema::fromJsonString($schemaString);
+        $result = $this->_validator->schemaValidation($data, $schema);
+        $this->assertFalse($result->isValid());
     }
 
     protected function _writeObjectToLog($object, $logName = 'some_log.txt')
