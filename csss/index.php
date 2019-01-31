ec5ec88ec18cc811b0dde8d017d75a4553d2dac9
akirmas <andrii@gobe-mark.com>
2019-01-31 12:36:38 +0200

    Delete reduntant css folder

--- csss/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(__DIR__.'/../utils.php');
-
-$css = !array_key_exists('css', $_GET)
-  // Trick to cause error - 'out of folder' in this case
-  ? '..'
-  : $_GET['css'].'.css';
-fileDelivery(__DIR__, $css, 'text/css');
-exit;
