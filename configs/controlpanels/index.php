a9eec70760e37804bcaf3e94b1043318f912ba2e
akirmas <andrii@gobe-mark.com>
2019-02-18 15:55:50 +0200

    Delete outdated and reduntant

--- configs/controlpanels/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(__DIR__.'/../../utils.php');
-
-$json = !array_key_exists('json', $_GET)
-  // Trick to cause error - 'out of folder' in this case
-  ? '..'
-  : $_GET['json'].'.json';
-fileDelivery(__DIR__, $json, 'application/json');
-exit;
