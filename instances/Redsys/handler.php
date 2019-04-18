e7839ff601ce12b7afa3e8814ea4c86f601a58bc
akirmas <andrii@gobe-mark.com>
2019-04-18 17:47:15 +0300

    #29 Add calculation of orderId in handler

--- instances/Redsys/handler.php
+++ instances/Redsys/handler.php
@@ -2,10 +2,29 @@
 require_once(__DIR__.'/../../CycleHandler.php');
 require_once(__DIR__.'/../../utils/assoc.php');
 require_once(__DIR__.'/../functions.php');
+require_once(__DIR__.'/../../utils/stringmath.php');
 
 class Redsys extends CycleHandler {
+  static function onRequestFilled($env, $request) {
+    $orderId = baseChangeString(
+      //TODO: pick from env
+      '()*+,./-0123456789:;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~',
+      '0123456789',
+      \assoc\getValue($request, 'autoincrement')
+    );
+
+    return [
+      'orderId' => baseChangeString(
+        //TODO: pick from env
+        '()*+,./0123456789:;?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_abcdefghijklmnopqrstuvwxyz{|}~',
+        '0123456789',
+        \assoc\getValue($request, 'autoincrement')
+      )
+    ];
+  }
   static function onResponseRaw($env, $response, $request) {
     $key = $response['account:key'];
+    //TODO: pick from env
     foreach(['account:key', 'version', '$output', 'gateway'] as $redudantKey)
       unset($response[$redudantKey]);
     return [
