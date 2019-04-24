b6aeb523418588c5e8aecc611fadb9c6ac313f8f
akirmas <andrii@gobe-mark.com>
2019-04-24 13:44:01 +0300

    Add paypal handler

--- instances/Paypal/handler.php
+++ instances/Paypal/handler.php
@@ -2,14 +2,22 @@
 require_once(__DIR__.'/../../CycleHandler.php');
 require_once(__DIR__.'/../../utils/assoc.php');
 
-class Isracard extends CycleHandler {
-  static function onResponseRaw($env,$response, $request) {
-    return !\assoc\keyExists($env, ['engine', 'gateway'])
-    ? []
-    : [
-      'quizUrl' => \assoc\getValue($response, ['engine', 'gateway'])
-      .'?'.http_build_query($response)
-    ];
+class Paypal extends CycleHandler {
+  static function onResponseRaw($env, $response, $request) {
+    $inputs = array_filter(
+        $response,
+        function ($key) {
+          return !in_array($key, [
+            'form:action',
+            'form:method',
+            'form:target'
+          ]);
+        },
+        ARRAY_FILTER_USE_KEY
+      );
+      return [
+        'form:inputs' => json_encode($inputs, JSON_UNESCAPED_SLASHES)
+      ];
   }
 }
 
