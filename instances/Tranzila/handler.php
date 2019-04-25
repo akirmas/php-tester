165c21b1fcf8919ee03d52e0cdde61ab994495d8
akirmas <andrii@gobe-mark.com>
2019-04-25 11:24:33 +0300

    Recover tranzila's handler lost in 33bd456cca6790a69a25d0a4859b75e440b7b3a1#diff-6fa9325472628103b7299e0e03a3cb93

---
+++ instances/Tranzila/handler.php
@@ -0,0 +1,15 @@
+@@ -1,16 +0,0 @@
+<?php
+require_once(__DIR__.'/../../CycleHandler.php');
+require_once(__DIR__.'/../../utils/assoc.php');
+class Tranzila extends CycleHandler {
+  static function onResponseRaw($env, $response, $request) {
+    $responseCode = !\assoc\keyExists($response, 'Response')
+    ? 0
+    : $response['Response'];
+    return [
+      'ResponseClone' => $responseCode,
+      'ResponseClone2' => $responseCode
+    ];
+  }
+}
