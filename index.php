3ed315ce4c5ff7fb85c4fbada4e97ac5b7d57354
Andrii <banderlog17@gmail.com>
2019-05-03 15:47:22 +0300

    Move test files to separate folder.

--- index.php
+++ index.php
@@ -1,347 +1,69 @@
 <?php
-set_time_limit(0);
-ini_set('max_execution_time', 0);
+$failedProject = false;
+$report = [];
+$opts = getopt('', ['url:', 'script:', 'all']);
+$opts['all'] = array_key_exists('all', $opts);
+$opts['script'] = array_key_exists('script', $opts) ? $opts['script'] : 'index';
+$opts['url'] = array_key_exists('url', $opts) ? $opts['url'] : null;
 
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET');
-header('Access-Control-Allow-Headers: origin, x-requested-with, Content-Type, Date, Request-Date');
+$scriptPaths = [$opts['script']];
+forEach($scriptPaths as $scriptPath) {
+  $testPath = __DIR__.'/'.preg_replace('/\.php$/i', '', $scriptPath).'.test.json';
+  $tests = json_decode(file_get_contents($testPath), true);
+  $failedScript = false;
+  $testNames = array_keys($tests);
+  $report[$scriptPath] = array_map(
+    function($name) use ($scriptPath, $tests, &$failedScript, $opts) {
+      //TODO: set up 'style' of test - CLI, HTTP/GET, HTTP/POST
 
-require_once(__DIR__.'/utils/assoc.php');
-require_once(__DIR__.'/handler.php');
-require_once(__DIR__.'/instances/functions.php');
+      $responseText = is_null($opts['url'])
+      ? callTest($scriptPath, $tests[$name][0])[0]
+      : file_get_contents($opts['url']."$scriptPath.php?".http_build_query($tests[$name][0]));
 
-$commonHandler = 'CommonHandler';
+      $response = json_decode($responseText, true);
+      if (is_null($response))
+        $response = $responseText;
 
-$system = [
-  'tmstmp' => tmstmp(),
-  'scriptPath' => \assoc\getValue($_SERVER, 'SCRIPT_FILENAME', ''),
-  'referer:ip' => getClientIp(),
-  'referer:port' => \assoc\getValue($_SERVER, 'SERVER_PORT', ''),
-  'referer:method' => \assoc\getValue($_SERVER, 'REQUEST_METHOD', 'CLI'),
-  'referer:host' => \assoc\getValue($_SERVER, 'HTTP_HOST', ''),
-  'referer:url' => \assoc\getValue($_SERVER, 'HTTP_REFERER', '')
-];
+      $expected = $tests[$name][1];
+      $failedTest = !isSubset($response, $expected);
+      $failedScript = $failedScript || $failedTest;
+      $output = [$name =>
+        !$failedTest
+        ? true
+        : array(
+          "response" => $response,
+          "expected" => $expected
+        )
+      ];
+      if (!$opts['all'] && $failedTest)
+        exiting($failedTest, $output);
+      return $output;
+    },
+    $testNames
+  );
+  $failedProject = $failedProject || $failedScript;
+}
 
-if ($system['referer:method'] === 'OPTIONS')
-  closeAndExit();
+exiting($failedProject, $report);
 
-$input = json_decode(file_get_contents('php://input'), true);
-if (!is_array($input))
-  $input = [];
-$argv = json_decode(\assoc\getValue($_SERVER, ['argv', 1], "[]"), true);
-$input = $_REQUEST + $argv + $input;
-//$input = json_decode(file_get_contents(__DIR__.'/index.test.json'), true)['paypal_andrain'][0];
-
-//NB! HARDCODE
-if (\assoc\keyExists($input, 'cc:number'))
-  $input['cc:number'] = preg_replace('/[^0-9]+/', '', $input['cc:number']);
-
-$processPath = preg_replace('%(^/+|/+$)%', '',
-  \assoc\getValue($_SERVER, 'PATH_INFO',
-    \assoc\join2('/',
-     \assoc\getValues($input, ['account', 'process'])
-    )
-  )
-);
-if (empty($processPath)) {
-  echo 1;
+function exiting($failed, $report = []) {
+  if ($failed)
+    exit("1\n".json_encode($report, JSON_PRETTY_PRINT, JSON_UNESCAPED_SLASHES)."\n");
+  echo 0;
   exit;
 }
-// The only field to be hardcoded - key 'account' will be used as it in 3rd parties, avoid ambiguity
-unset($input['account']);
-preg_match('%^[^/]+%', $processPath, $system['_account']);
-$system['_account'] = \assoc\getValue($system['_account'], 0);
-$system['process'] = $processPath;
 
-$input = $system + $input;
-
-$event = 'Request';
-$phase = 'Raw';
-
-if (!\assoc\keyExists($input, 'id')) $input['id'] = $input['tmstmp'];
-
-$processDir = mkdir2(__DIR__, '..', 'processes', $processPath, $input['id']);
-$logDir = mkdir2($processDir, $input['tmstmp']);
-$processDir = mkDir2($processDir, 'index');
-
-$ConfigDir = __DIR__.'/../configs';
-$steps = json_decode(file_get_contents(join('/', [
-  $ConfigDir, 'processes', $processPath.'.json'
-])), true);
-$strategy = \assoc\getValue(\assoc\keys($steps), 0, "anyOf");
-$steps = \assoc\getValue($steps, $strategy, [$steps]);
-$output = [];
-$filled = [];
-$requestData = [];
-forEach($steps as $step) {
-  $processor = is_array($step)
-  ? join('/', [$step['instance'], 'accounts', $step['account']])
-  : $step;
-  $handler = \assoc\getValue($step, 'instance', explode('/', $processor)[0]);
-    
-  //$handlerPath = $ConfigDir."/instances/$handler/handler.php";
-  $handlerPath2 = __DIR__."/instances/$handler/handler.php";
-  /*if (file_exists($handlerPath))
-    require_once($handlerPath);
-  else*/if (file_exists($handlerPath2))
-    require_once($handlerPath2);
-  else {
-    $handler = 'CycleHandler';
-    require_once(__DIR__."/$handler.php");
-  }
-
-  $instance = \assoc\mergeJsonPaths("{$ConfigDir}/instances", $processor);
-
-  $request = \assoc\getValue($instance, 'request', []);
-  $response = \assoc\getValue($instance, 'response', []);
-  
-  $filled = \assoc\merge(
-    \assoc\getValue($request, 'defaults', []),
-    $input,
-    \assoc\getValue($request, 'overrides', [])
-  );
-
-  $event = 'Request';
-  $phase = 'Filled';
-  
-  if(\assoc\keyExists($request, ['engine', 'autoincrement'])) {
-    $incrementKey = \assoc\getValue($request, ['engine', 'autoincrement']);
-    $filled['autoincrement'] = autoincrement($incrementKey);
-  }
-  
-  $filled = fireEvent(
-    (\assoc\getValue($request, ['engine', 'history'], false))
-    ? \assoc\merge($requestData, $output, $filled)
-    : $filled
-  );
-  
-  $filled = fillValues(
-    $filled,
-    \assoc\merge(
-      $filled,
-      \assoc\getValue($request, 'engine')
-    )
-  );
-
-  $event = 'Request';
-  $phase = 'Calced';
-
-  //Keep unified vocabulary in schema keys
-  $requestData = (\assoc\getValue($request, ['engine', 'sourceIsAPI'], false))
-  ? \assoc\mapValues(
-    \assoc\mapKeys(
-      $filled,
-      \assoc\flip(\assoc\getValue($request, 'fields', [])),
-      false,
-      false
-    ),
-    \assoc\getValue($request, 'values', []),
-    true
-  ) : \assoc\mapKeys(
-    \assoc\mapValues(
-      $filled,
-      \assoc\getValue($request, 'values', []),
-      true
-    ),
-    \assoc\getValue($request, 'fields', []),
-    false,
-    false
-  );
-
-  $event = 'Request';
-  $phase = 'Formed';
-
-  $request['engine']['gateway'] = formatString(
-    \assoc\getValue($request, ['engine', 'gateway'], ''),
-    $requestData + $filled +  \assoc\getValue($request, ['engine'], [])
-  );
-
-  $cachePath = '';
-  if (\assoc\getValue($request, ['engine', 'cache'], false)) {
-    $cacheDir = mkdir2(__DIR__, '..', 'cached', $processPath);
-    $cachePath = "{$cacheDir}/"
-    .hash("md5",
-      json_encode(\assoc\getValue($request, ['engine'], []))
-      .json_encode($requestData)
-    );
-    if (file_exists($cachePath))
-      $request['engine']['method'] = 'useCached';
-  }
-  switch(\assoc\getValue($request, ['engine', 'method'], '')) {
-    case 'PATCH':
-    case 'POST':
-      $ch = curl_init(\assoc\getValue($request, ['engine', 'gateway'], ''));
-      curl_setopt_array($ch,
-        [
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_FRESH_CONNECT => true,
-          CURLOPT_CUSTOMREQUEST => \assoc\getValue($request, ['engine', 'method'], ''),
-          CURLOPT_HEADER => 1,
-          //CURLOPT_VERBOSE => 1,
-          CURLOPT_POSTFIELDS => json_encode($requestData, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE),
-          CURLOPT_HTTPHEADER => array_merge(
-            [
-              'Request-Date: '. gmdate('D, d M Y H:i:s T'),
-              'Date: '. gmdate('D, d M Y H:i:s T')
-            ],
-            \assoc\getValue($request, ['engine', 'headers'], [])
-          )
-        ]
-      );
-      $responseText = curl_exec($ch);
-      if ($responseText === false) 
-        throw new Exception(curl_error($ch), curl_errno($ch));
-      $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-      curl_close($ch);
-      
-      //Despite declared content-type - APIs love to return .html pages on errors
-      $htmlResp = '<!doctype html>';
-      if ($htmlResp === strtolower(substr($responseText, 0, strlen($htmlResp))))
-        file_put_contents("$processDir/error.html", $responseText);
-
-      $header = substr($responseText, 0, $header_size);
-      $responseText = substr($responseText, $header_size);
-      break;
-    case 'GET':
-      $responseText = file_get_contents(
-        \assoc\getValue($request, ['engine', 'gateway'], '')
-        .'?'
-        .http_build_query($requestData)
-      );      
-      break;
-    case 'useCached':
-      $responseText = file_get_contents($cachePath);      
-      break;
-    case 'no_curl':
-      $response['engine']['contentType'] = $request['engine']['method'];
-      break;
-    default: {
-      http_response_code(501);
-      closeAndExit('not impelemented');
-    }
-  }
-
-  if ($cachePath !== '' && \assoc\getValue($request, ['engine', 'method'], '') !== 'useCached')
-    file_put_contents($cachePath, $responseText);
-  
-  switch($response['engine']['contentType']) {
-    case 'application/x-www-form-urlencoded':
-      parse_str(
-        $responseText,
-        $responseData
-      );
-      break;
-    case 'no_curl':
-      $responseData = $requestData;
-      break;
-    case 'application/json':
-      $responseData = json_decode($responseText, true);
-      if ($responseData !== null && \assoc\isESObject($responseData)) 
-        break;
-    case 'text/plain':
-    default:
-      $responseData = ['response' => $responseText];
-  }
-
-  $output = \assoc\merge(
-    \assoc\getValue($response, 'defaults', []),
-    $responseData,
-    \assoc\getValue($response, 'overrides', [])
-  );
-
-  $event = 'Response';
-  $phase = 'Raw';
-
-  $output = fireEvent($output, $output);
-
-  $output = \assoc\mapValues(
-    \assoc\mapKeys(
-      $output,    
-      \assoc\getValue($response, 'fields', []),
-      true,
-      false
-    ),
-    \assoc\getValue($response, 'values', []),
-    true
-  );
-
-  $output = $system
-  + ['processor' => $processor]
-  + $output;
-  $output = fillValues(
-    $output,
-    \assoc\merge($output, $filled, \assoc\getValue($response, 'engine'))
-  );
-
-  $event = 'Response';
-  $phase = 'Formed';
-  $output = fireEvent($output, $filled);
-
-  $success = assoc\getValue($output, 'success');
-  if (
-    // until first TRUE
-    in_array($strategy, ['anyOf', 'oneOf'], true)
-    && in_array($success, [1, '1'], true)
-    // until first FALSE
-    || in_array($strategy, ['allOf'], true)
-    && in_array($success, [0, '0'], true)
-    // 'just do it' is 'anyOf' - and any value of $strategy (even 'Nike')
-  )
-    break;
+function callTest($php, $params) {
+  $params = '"'.preg_replace('/["]/', '\\\\$0', json_encode($params)).'"';
+  //$params = escapeshellarg($params);
+  $php = preg_replace('/\.php/', '', $php).'.php';
+  $output = null;
+  exec("php $php $params", $output);
+  return $output;
 }
 
-$responseContentType = \assoc\getValue($response, ['output', 'contentType'], 'application/json');
-header("Content-Type: {$responseContentType}; charset=utf-8");
-
-switch($responseContentType) {
-  case 'text/html': 
-    echo \assoc\getValue($output, \assoc\getValue($response, ['output', 'contentFrom']));
-    break;
-  case 'application/json': 
-    echo json_encode($output, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    break;
-  default:
-}
-
-
-function fireEvent(...$data) {
-  global $event, $phase, $handler, $logDir, $processDir, $commonHandler, $request, $response;
-  $step = $event === 'Request'
-  ? $request
-  : (
-    $event === 'Response'
-    ? $response
-    : []
-  );
-  // TODO: Step is env object, not string
-  $data[0] = \assoc\merge(
-    call_user_func(
-      ["\\$commonHandler", "on$event$phase"],
-      ...array_merge([$step], $data)
-    ),
-    $data[0]
-  );
-  $data[0] = \assoc\merge(
-    call_user_func(
-      ["\\$handler", "on$event$phase"],
-      ...array_merge([$step], $data)
-    ),
-    $data[0]
-  );
-  $dirs = [$logDir, $processDir];
-  $dataJson = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-  foreach ($dirs as $dir) {
-    file_put_contents(
-      mkdir2($dir, $handler)."/$event$phase.json",
-      $dataJson
-    );
-  }
-  if ($event === 'Response' && $phase = 'Formed') {
-    file_put_contents(
-      "$processDir/$handler/index.json",
-      $dataJson
-    );
-  }
-  return $data[0];
+function isSubset($set, $sub) {
+  return is_array($set) && is_array($sub)
+  ? $sub === array_intersect_assoc($sub, $set)
+  : $set === $sub;
 }
--- psps_json_validation_test/local_test_data/configs/instances/Tranzila/handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(__DIR__.'/../../CycleHandler.php');
-
-class Tranzila extends CycleHandler {
-  static function onResponseRaw(object $env, object $response, object $request): object{
-    return (object) array('ResponseClone' => $response->Response);
-  }
-}
-
